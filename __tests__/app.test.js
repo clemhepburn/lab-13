501060b7299ae566e462155b25ef8967615f3846
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-21 15:58:10 -0700

    delete some more unneeded files, create data files, test location function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,25 @@
 import supertest from 'supertest';
 import app from '../lib/app.js';
 const request = supertest(app);
+import locationData from '../data/location';
+import { formatLocation, formatWeather, formatYelp } from '../lib/munge-utils.js';
 
 describe('API Data Munging Functions', () => {
+
+  const expectedLocation = {
+    formatted_query: 'Portland, Multnomah County, Oregon, USA',
+    latitude: '45.5202471',
+    longitude: '-122.6741949'
+  };
+
+    
+  test('munges location data', async () => {
+    
+    const output = formatLocation(locationData);
+
+    expect(output).toEqual(expectedLocation);
+
+  });
 
 
 });
