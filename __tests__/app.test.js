70e841c8e3edbb58535ff10dd92b1dbd4c7cc1ec
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-21 16:10:42 -0700

    maging yelp passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ import app from '../lib/app.js';
 const request = supertest(app);
 import locationData from '../data/location';
 import weatherData from '../data/weather';
+import yelpData from '../data/yelp';
 import { formatLocation, formatWeather, formatYelp } from '../lib/munge-utils.js';
 
 describe('API Data Munging Functions', () => {
@@ -42,6 +43,36 @@ describe('API Data Munging Functions', () => {
     const output = formatWeather(weatherData);
 
     expect(output).toEqual(expectedWeather);
+  });
+
+  const expectedYelp = [
+    {
+      name: 'Luc Lac',
+      image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg',
+      price: '$$',
+      rating: 4.0,
+      url: 'https://www.yelp.com/biz/luc-lac-portland-7?adjust_creative=SIP1Dfwy1M0HWzdgGg9H9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SIP1Dfwy1M0HWzdgGg9H9g'
+    },
+    {
+      name: 'Screen Door',
+      image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/lqmMYlLRV-aoH73koWA6Ew/o.jpg',
+      price: '$$',
+      rating: 4.5,
+      url: 'https://www.yelp.com/biz/screen-door-portland?adjust_creative=SIP1Dfwy1M0HWzdgGg9H9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SIP1Dfwy1M0HWzdgGg9H9g'
+    },
+    {
+      name: 'Q Restaurant & Bar',
+      image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/jAH0XyZe5N8YTrOy71SuJg/o.jpg',
+      price: '$$',
+      rating: 4.5,
+      url: 'https://www.yelp.com/biz/q-restaurant-and-bar-portland?adjust_creative=SIP1Dfwy1M0HWzdgGg9H9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SIP1Dfwy1M0HWzdgGg9H9g'
+    }
+  ];
+
+  test('munging the yelp data', () => {
+    const output = formatYelp(yelpData);
+
+    expect(output).toEqual(expectedYelp);
   });
 
 
