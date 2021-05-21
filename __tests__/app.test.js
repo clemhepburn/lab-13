0d01fdecfb3428ccd5101927c3e329e814158fd3
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-21 16:04:15 -0700

    passing weather munge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import supertest from 'supertest';
 import app from '../lib/app.js';
 const request = supertest(app);
 import locationData from '../data/location';
+import weatherData from '../data/weather';
 import { formatLocation, formatWeather, formatYelp } from '../lib/munge-utils.js';
 
 describe('API Data Munging Functions', () => {
@@ -19,6 +20,28 @@ describe('API Data Munging Functions', () => {
 
     expect(output).toEqual(expectedLocation);
 
+  });
+
+  const expectedWeather = [
+
+    {
+      forecast: 'Broken clouds',
+      time: '2021-05-12'
+    },
+    {
+      forecast: 'Few clouds',
+      time: '2021-05-13'
+    },
+    {
+      forecast: 'Moderate rain',
+      time: '2021-05-14'
+    }
+  ];
+
+  test('weather data munge function', async () => {
+    const output = formatWeather(weatherData);
+
+    expect(output).toEqual(expectedWeather);
   });
 
 
